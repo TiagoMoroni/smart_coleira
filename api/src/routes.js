0dd9e8791d6027b63c70447cac641082858e5a92
desenvolvimento02jocc <Desenvolvimento02@jocc.com.br>
2021-09-29 16:22:51 -0300

    fix on updates

--- api/src/routes.js
+++ api/src/routes.js
@@ -9,17 +9,17 @@ const Auth = require("./utils/auth")
 
 router.get('/users', UserController.index)
 router.post('/users', UserController.store)
-router.put('/users', UserController.update)
+router.put('/users/:userId', UserController.update)
 router.delete('/users/:userId', UserController.delete)
 
 router.get('/animals', AnimalController.index)
 router.post('/animals', AnimalController.store)
-router.put('/animals', AnimalController.update)
+router.put('/animals/:animalId', AnimalController.update)
 router.delete('/animals/:animalId', AnimalController.delete)
 
 router.get('/collars', CollarController.index)
 router.post('/collars', CollarController.store)
-router.put('/collars', CollarController.update)
+router.put('/collars/:collarId', CollarController.update)
 router.delete('/collars/:collarId', CollarController.delete)
 
 router.post('/auth', Auth.auth)
